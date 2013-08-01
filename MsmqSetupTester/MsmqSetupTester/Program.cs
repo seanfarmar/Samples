f0312121e7e81206650f9de373153430520c7f4a
sfarmar <s.farmar@gmail.com>
2013-08-01 10:38:41 +0000

    MsmqSetup test harness

---
+++ MsmqSetupTester/MsmqSetupTester/Program.cs
@@ -0,0 +1,24 @@
+﻿using NServiceBus.Setup.Windows.Msmq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsmqSetupTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press 'Enter' to install MSMQ.To exit, Ctrl + C");
+
+            while (Console.ReadLine() != null)
+            {
+                var result =  MsmqSetup.StartMsmqIfNecessary(true);
+
+                Console.WriteLine("install result was: {0}", result);
+            }
+        }
+    }
+}
