62b60152b46f0f0e2b02861644eee1048e269e38
sfarmar <s.farmar@gmail.com>
2013-08-02 14:16:04 +0100

    running send local non transactional with a non transaction queues (don't know why you would do that...)

--- HalloWorldNoTransactions/HalloWorld.MyEndpoint/EndpointConfig.cs
+++ HalloWorldNoTransactions/HalloWorld.MyEndpoint/EndpointConfig.cs
@@ -4,19 +4,11 @@ namespace HalloWorld.MyEndpoint
 {
     public class EndpointConfig : IConfigureThisEndpoint, AsA_Client, IWantCustomInitialization
     {
-
         public void Init()
         {
             Configure.Transactions.Disable();
-        }
-    }
 
-    public class Unobtrusive : IWantToRunBeforeConfiguration
-    {
-        public void Init()
-        {
-            Configure
-                .Instance                
+            Configure.With()
                 .DefiningCommandsAs(n => n.Namespace != null && n.Namespace.Contains("Commands"))
                 .DefiningEventsAs(n => n.Namespace != null && n.Namespace.Contains("Events"));
         }
