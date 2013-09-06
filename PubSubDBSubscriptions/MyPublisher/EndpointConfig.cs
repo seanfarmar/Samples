63018623263842e98bc245aa475eab49d52732c7
sfarmar <s.farmar@gmail.com>
2013-09-06 10:58:37 +0100

    DB Subscription storage sample using nHibernate

---
+++ PubSubDBSubscriptions/MyPublisher/EndpointConfig.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus;
+
+namespace MyPublisher
+{
+    class EndpointConfig :  IConfigureThisEndpoint, AsA_Publisher,IWantCustomInitialization
+    {
+        public void Init()
+        {
+            Configure.With()
+                .DefaultBuilder()
+                .UseNHibernateSubscriptionPersister()
+                //this overrides the NServiceBus default convention of IEvent
+                .DefiningEventsAs(t => t.Namespace != null && t.Namespace.StartsWith("MyMessages"));}
+    }
+}
