7e298b6445988a6fe37ec83c03f9b0a414f3b9e0
sfarmar <s.farmar@gmail.com>
2013-12-17 20:46:46 +0200

    added pusub for 4.0.3

---
+++ NHPersistance/PubSub_4.0.3/MyPublisher/InititalizeSubscriptionStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NServiceBus;
+
+namespace MyPublisher
+{
+    public class InititalizeSubscriptionStorage : IWantCustomInitialization
+    {
+        public void Init()
+        {
+            NServiceBus.Configure.Instance
+               .UseNHibernateSubscriptionPersister();
+        }
+    }
+}
