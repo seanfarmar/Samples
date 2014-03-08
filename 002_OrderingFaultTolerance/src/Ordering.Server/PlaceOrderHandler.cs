68be97625913c66d5d9491074abc699536a8e530
sfarmar <s.farmar@gmail.com>
2014-03-08 14:18:08 +0000

    update to 4.4.2 and added SLR header count

--- 002_OrderingFaultTolerance/src/Ordering.Server/PlaceOrderHandler.cs
+++ 002_OrderingFaultTolerance/src/Ordering.Server/PlaceOrderHandler.cs
@@ -12,6 +12,14 @@
         {
             Console.WriteLine(@"Order for Product:{0} placed with id: {1}", message.Product, message.Id);
 
+            if (Bus.CurrentMessageContext.Headers.ContainsKey("Headers.Retries"))
+            {
+                var numberOfRetries = Bus.CurrentMessageContext.Headers[Headers.Retries];
+
+                Console.WriteLine("SLR is kicked in, Bus.CurrentMessageContext.Headers[Headers.Retries]:{0} id: {1}", numberOfRetries,
+                    message.Id);
+            }
+
             // throw new Exception("Uh oh - something went wrong....");
         }
     }
