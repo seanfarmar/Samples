f778a1242769801605c36667d03a8e3ca02e1e35
sfarmar <s.farmar@gmail.com>
2014-06-11 15:35:39 +0100

    Updated to rolling file logger

--- Log4NetFileLogger/Log4NetFileLogger/EndpointConfig.cs
+++ Log4NetFileLogger/Log4NetFileLogger/EndpointConfig.cs
@@ -7,6 +7,10 @@ namespace Log4NetFileLogger
 	    public void Init()
 	    {
             SetLoggingLibrary.Log4Net(log4net.Config.XmlConfigurator.Configure);
+
+            //var l = new log4net.Appender.RollingFileAppender();
+
+            //l.fi
 	    }
     }
 }
