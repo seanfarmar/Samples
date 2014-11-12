b876eff13979adc2480a321ab0c4f7c374b69cd2
sfarmar <s.farmar@gmail.com>
2014-11-12 11:20:58 +0000

    added a saga

--- DIUnitOfWorkV5Autofac/EndPoint/MyOtherCommandHandler.cs
+++ DIUnitOfWorkV5Autofac/EndPoint/MyOtherCommandHandler.cs
@@ -2,12 +2,12 @@
 {
 	using System;
 	using Messages.Commands;
-	using NServiceBus;
+	using NServiceBus.Saga;
 	using Raven.Client;
 	using Raven.Client.Document;
 
-    public class MyOtherCommandHandler : IHandleMessages<IMyOtherCommand>
-	{
+    public class MyOtherCommandHandler : Saga<MySagaData>,IAmStartedByMessages<IMyOtherCommand>
+    {
         public IDocumentSession Session { get; set; }
 
 		public void Handle(IMyOtherCommand message)
@@ -19,6 +19,10 @@
             Console.WriteLine("Session Id:({0}) - Saving MyOtherCommand to raven", ((DocumentSession)Session).Id);
 			Console.WriteLine("==========================================================================");
 
+		    Data.IdGuid = message.IdGuid;
+            Data.SomeString = message.Name;
+            Data.Throw = message.Throw;
+
             Session.Store(new OtherCommandData
             {
                 IdGuid = message.IdGuid,
@@ -27,5 +31,20 @@
 
             if (message.Throw) throw new Exception("Ho nos, MyOtherCommand, we have an issue...");
         }
-	}
+
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<MySagaData> mapper)
+        {
+            mapper.ConfigureMapping<MyOtherCommand>(s => s.IdGuid).ToSaga(m => m.IdGuid);
+        }
+    }
+
+    public class MySagaData : ContainSagaData
+    {
+        [Unique]
+        public Guid IdGuid { get; set; }
+
+        public bool Throw { get; set; }
+
+        public string SomeString { get; set; }
+    }
 }
