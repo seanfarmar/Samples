1952970fa343dc163eea919e683cd8820bc12067
sfarmar <s.farmar@gmail.com>
2015-12-03 10:24:52 +0000

    updated log4net sample

--- Log4NetFileLogger/Log4NetFileLogger/EndpointConfig.cs
+++ Log4NetFileLogger/Log4NetFileLogger/EndpointConfig.cs
@@ -1,16 +1,26 @@
-namespace Log4NetFileLogger 
+
+namespace Log4NetFileLogger
 {
     using NServiceBus;
 
-	public class EndpointConfig : IConfigureThisEndpoint, IWantCustomLogging, AsA_Server
+    /*
+		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
+		can be found here: http://particular.net/articles/the-nservicebus-host
+	*/
+    public class EndpointConfig : IConfigureThisEndpoint
     {
-	    public void Init()
-	    {
-            SetLoggingLibrary.Log4Net(log4net.Config.XmlConfigurator.Configure);
-
-            //var l = new log4net.Appender.RollingFileAppender();
-
-            //l.fi
-	    }
+        public void Customize(BusConfiguration configuration)
+        {
+            // NServiceBus provides the following durable storage options
+            // To use RavenDB, install-package NServiceBus.RavenDB and then use configuration.UsePersistence<RavenDBPersistence>();
+            // To use SQLServer, install-package NServiceBus.NHibernate and then use configuration.UsePersistence<NHibernatePersistence>();
+            
+            // If you don't need a durable storage you can also use, configuration.UsePersistence<InMemoryPersistence>();
+            // more details on persistence can be found here: http://docs.particular.net/nservicebus/persistence-in-nservicebus
+            
+            //Also note that you can mix and match storages to fit you specific needs. 
+            //http://docs.particular.net/nservicebus/persistence-order
+            configuration.UsePersistence<InMemoryPersistence>();
+        }
     }
 }
